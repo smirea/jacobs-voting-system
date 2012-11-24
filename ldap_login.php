329a0c6640e6520c19fefbaed5253e6fe037e8e9
Frédérique Mittelstaedt <f.mittelstaedt@jacobs-university.de>
2012-11-24 14:16:43 +0100

    Added ldap_login script
    LDAP login script universal, without SSL

---
+++ ldap_login.php
@@ -0,0 +1,29 @@
+<?php
+$host = "jacobs.jacobs-university.de";
+$port = 389;
+$base_dn = "DC=jacobs,DC=jacobs-university,DC=de";
+$user_dn = "OU=active,OU=Users,OU=CampusNet,DC=jacobs,DC=jacobs-university,DC=de";
+
+$username = mysql_escape_string($_POST["username"]);
+$password = mysql_escape_string($_POST["password"]);
+
+if (($username=="") || ($password==""))
+    $success = false;
+else {
+    error_reporting(-1);
+    $ldap_conn = ldap_connect($host);
+    ldap_set_option($ldap_conn, LDAP_OPT_PROTOCOL_VERSION, 3);  //Set the LDAP Protocol used by your AD service
+    ldap_set_option($ldap_conn, LDAP_OPT_REFERRALS, 0);         //This was necessary for my AD to do anything
+    $success = ldap_bind($ldap_conn,
+                    $username."@jacobs.jacobs-university.de",
+                    $password);
+    ldap_unbind($ldap_conn);
+}
+
+error_reporting(0);
+
+echo "success";
+
+session_destroy();
+
+?>
