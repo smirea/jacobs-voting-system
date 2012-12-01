e6abaefa9d4983f6cd7fcef0e44457bef6bfde66
Stefan Mirea <steven.mirea@gmail.com>
2012-12-01 17:49:07 +0100

    dropping tables if they exist

--- database.sql
+++ database.sql
@@ -4,22 +4,14 @@
 --create user jPerson@'localhost' identified by 'jacobsRulz';
 --grant all privileges on VotingSystem.* to 'jPerson'@'localhost' IDENTIFIED BY 'jacobsRulz';
 
-DROP TABLE IF EXISTS `Elections`;
-CREATE TABLE `Elections`(
-  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `user_id` INT NOT NULL,
-  `label` VARCHAR(128) NOT NULL,
-  `vote` INT NOT NULL,
-  INDEX `user_id_index` (user_id),
-  INDEX `label_index` (label),
-  INDEX `vote_index` (vote)
-) ENGINE = MYISAM ;
 
+DROP TABLE IF EXISTS `poll`;
 CREATE TABLE `poll`(
   `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `type` VARCHAR(64) NOT NULL
 ) ENGINE = MYISAM ;
 
+DROP TABLE IF EXISTS `options`;
 CREATE TABLE `options`(
   `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `poll_id` INT NOT NULL,
@@ -27,6 +19,7 @@ CREATE TABLE `options`(
   `value` INT NOT NULL DEFAULT '0'
 ) ENGINE = MYISAM ;
 
+DROP TABLE IF EXISTS `votes`;
 CREATE TABLE `votes`(
   `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `user_id` INT NOT NULL,
