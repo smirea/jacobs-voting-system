f607ce0fcdd1825d148dc6d36b88e0e25e2daa13
Stefan Mirea <steven.mirea@gmail.com>
2012-12-06 23:44:30 +0100

    added php doc

--- classes/VoteModel.php
+++ classes/VoteModel.php
@@ -4,14 +4,25 @@
 
   require_once 'classes/Model.php';
 
+
   class VoteModel {
 
   	public $table_name;
 
+    /**
+     * Constructor for the Model
+     * @param {String} $table_name the name of the table
+     */
   	function __construct($table_name) {
   		$this->table_name = $table_name;
   	}
 
+    /**
+     * Inserts a vote into the table
+     * @param {Int} $poll_id the id of the poll
+     * @param {Int} $user_id the id of the user
+     * @return {Bool}
+     */
   	public function add_vote($poll_id, $user_id) {
   		return mysql_query("INSERT INTO `".$this->table_name."` (user_id, poll_id) VALUES ('$user_id','$poll_id');");
   	}
