aa96f77bd70eb8b18aaccec30f41d0bcbec51d22
Stefan Mirea <steven.mirea@gmail.com>
2012-12-07 00:33:17 +0100

    added insert/update/select/delete methods

--- classes/Model.php
+++ classes/Model.php
@@ -7,8 +7,21 @@
       $this->table_name = $table_name;
     }
 
-    public function sql ($query) {
-      
+
+    public function select ($columns, $query) {
+      return mysql_query("SELECT $columns FROM ".$this->table_name." $query");
+    }
+
+    public function insert ($query) {
+      return mysql_query("INSERT INTO ".$this->table_name." $query");
+    }
+
+    public function update ($query) {
+      return mysql_query("UPDATE ".$this->table_name." $query");
+    }
+
+    public function delete ($query) {
+      return mysql_query("DELETE FROM ".$this->table_name." $query");
     }
   }
 
