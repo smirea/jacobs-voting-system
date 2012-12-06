78c92e9790ae421826fe44e49c25f27d8a188529
smirea <steven.mirea@gmail.com>
2012-12-07 00:05:37 +0100

    OptionModel.php looking all nice and shit. I am Stefan and I am cool. Herp derp.

--- classes/OptionModel.php
+++ classes/OptionModel.php
@@ -12,12 +12,22 @@
       parent::__construct($table_name);
     }
 
-    
-    public function add_option($poll_id, $option_name, $value) {
-      return mysql_query("INSERT INTO `".$this->table_name."` (`poll_id`, `option_name`, `value`) VALUES ('".$poll_id."', '".$option_name."', '".$value."'); ");
+    /**
+     * Add option to the database
+     * @param {Int} $poll_id is the id of the poll to which option should belong
+     * @param {String} $option_name is the name of option 
+     * @return {Bool} 
+     */    
+    public function add_option($poll_id, $option_name) {
+      return mysql_query("INSERT INTO `".$this->table_name."` (`poll_id`, `option_name`, `value`) VALUES ('$poll_id', '$option_name', '0'); ");
     }
 
-    // To get results of the vote -.D
+   /**
+    * Returns vote result for desired option
+    * @param {Int} $poll_id is the id of the poll to which option belongs
+    * @param {String} $option_name is the name of the option
+    * @return {Int}
+    * */
     public function option_value($poll_id, $option_name) {
 
       $value = mysql_fetch_assoc(mysql_query("SELECT value FROM options WHERE poll_id='$poll_id' AND option_name='$option_name';"));
@@ -25,7 +35,12 @@
       return $value['value'];
     } 
 
-    // To increment the vote by one -.D
+    /**
+     * Adds one to the option value (aka. VOTE)
+    * @param {Int} $poll_id is the id of the poll to which option belongs
+    * @param {String} $option_name is the name of the option
+    * @return {Bool}
+    * */
     public function increment_value($poll_id, $option_name) {
       
       $newValue = $this->option_value($poll_id, $option_name) + 1;
