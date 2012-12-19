b62e2b8842fc48cc6498f6d87916f347dedd852e
smirea <steven.mirea@gmail.com>
2012-12-19 05:41:09 +0100

    index is now THE generic controller. To display a view, go to index.php?q=view_name.php

--- index.php
+++ index.php
@@ -2,10 +2,13 @@
 
   require_once 'config.php';
   require_once 'utils.php';
+  require_once DIR_CLASSES . DS . 'Error.php';
+  require_once DIR_CLASSES . DS . 'Output.php';
 
   if (!isset($_GET['q'])) {
     output_error('No query specified');
   }
+
   $file = $_GET['q'];
 
   if (!file_exists(DIR_VIEWS . DS . $file)) {
@@ -13,5 +16,7 @@
   }
 
   require_once DIR_VIEWS . DS . $file;
+
+  Output::done();
 
 ?>
