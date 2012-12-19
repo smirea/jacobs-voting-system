f9ed757cfd105a7797756842c5807ea868d28d2b
smirea <steven.mirea@gmail.com>
2012-12-19 05:44:13 +0100

    added a pretty_print version of json_encode

--- utils.php
+++ utils.php
@@ -1,9 +1,11 @@
 <?php
 
+  require_once 'config.php';
+
   /**
    * @brief check if an array is associative or not
    * @param {array} $array
-   * @return {bool} the result as a boolean 
+   * @return {bool} the result as a boolean
    */
   function is_assoc(array $array){
     if( !is_numeric( array_shift( array_keys( $array ) ) ) ){
@@ -11,7 +13,7 @@
     }
     return false;
   }
-  
+
   /**
    * @brief Outputs a JSON with the proper headers from the given array
    * @warning This function terminates the execution (runs exit())
@@ -24,9 +26,9 @@
       header('Content-type:application/json');
       header('Content-attributes: application/json; charset=ISO-8859-15');
     }
-    exit( json_encode( $arr ) );
+    exit(DEBUG ? json_encode_indent($arr) : json_encode($arr));
   }
-  
+
   /**
    * @brief Outputs an error JSON with the format {"error" => $message}
    * @warning This function terminates the execution (runs exit())
@@ -35,7 +37,7 @@
   function output_error( $message ){
     json_output( array( 'error' => $message ) );
   }
-  
+
   /**
    * @brief Check if condition is false, in which case run output_error( of_the_message )
    * @warning This function the execution (runs exit())
@@ -47,7 +49,7 @@
       output_error( $message );
     }
   }
-  
+
   /**
    * @brief For each key in keys, apply e_assert( isset($arr[$key]) );
    * @param {array} $arr the array to check into
@@ -66,7 +68,7 @@
       e_assert( isset( $arr[$k] ), $v );
     }
   }
-  
+
   function sql_to_json_output( $q ){
     if( $q ){
       json_output( sql_to_array( $q ) );
@@ -74,9 +76,9 @@
       output_error( mysql_error() );
     }
   }
-  
+
   /**
-   * @brief Takes a mysql resource and returns a list of associative arrays 
+   * @brief Takes a mysql resource and returns a list of associative arrays
    *          with the results (one for each row)
    * @param {MySQL} $sql the resource to use
    * @param {} $key
@@ -97,5 +99,53 @@
       return array();
     }
   }
+
+
+  /**
+   * Convert an object to a JSON string with indentation
+   * @param  Mixed   $in
+   * @param  integer $indent
+   * @param  boolean $from_array
+   * @return String
+   */
+  function json_encode_indent ($in, $indent = 0, $from_array = false) {
+    $_myself = __FUNCTION__;
+    $indentation_string = '  ';
+    $_escape = function ($str) {
+      return preg_replace("!([\b\t\n\r\f\"\\'])!", "\\\\\\1", $str);
+    };
+
+    $out = '';
+
+    foreach ($in as $key=>$value) {
+      $out .= str_repeat($indentation_string, $indent + 1);
+      $out .= "\"".$_escape((string)$key)."\": ";
+
+      if (is_object($value) || is_array($value)) {
+        $out .= "\n";
+        $out .= $_myself($value, $indent + 1);
+      } elseif (is_bool($value)) {
+        $out .= $value ? 'true' : 'false';
+      } elseif (is_null($value)) {
+        $out .= 'null';
+      } elseif (is_string($value)) {
+        $out .= "\"" . $_escape($value) ."\"";
+      } else {
+        $out .= $value;
+      }
+      $out .= ",\n";
+    }
+
+    if (!empty($out)) {
+      $out = substr($out, 0, -2);
+    }
+
+    $brackets = is_assoc($in) ? array('{', '}') : array('[', ']');
+
+    $out = str_repeat($indentation_string, $indent) . $brackets[0] . "\n" . $out;
+    $out .= "\n" . str_repeat($indentation_string, $indent) . $brackets[1];
+
+    return $out;
+}
 
 ?>
