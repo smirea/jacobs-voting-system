3cbc8687c791d1c9894ffe3b4039bc13172af52d
Stefan Mirea <steven.mirea@gmail.com>
2013-01-09 12:33:53 +0200

    reindented

--- classes/Error.php
+++ classes/Error.php
@@ -1,81 +1,81 @@
 <?php
 
-/**
- * Generic error classs
- */
-class Error {
-
-  protected $message;
-
   /**
-   * Initializes a generic error
-   * @param string $message
+   * Generic error classs
    */
-  public function __construct ($message = 'An error has occured') {
-    $this->message = $message;
-  }
+  class Error {
 
-  /**
-   * Converts all the error information into an array to be used in output
-   */
-  public function to_object () {
-    return array(
-      'type' => get_class($this),
-      'message' => $this->message
-    );
-  }
+    protected $message;
 
-  /**
-   * Short description of the error
-   */
-  public function __toString () {
-    return $this->message;
-  }
+    /**
+     * Initializes a generic error
+     * @param string $message
+     */
+    public function __construct ($message = 'An error has occured') {
+      $this->message = $message;
+    }
 
-}
-
-/**
- * Used to abstract Model Errors
- */
-class DatabaseError extends Error {
-  protected $model;
-
-  /**
-   * Initializes a DatabaseError
-   * @param {Mode} $model   An instance of Model on which the error occured
-   * @param string $message A custom description of the error
-   */
-  public function __construct ($model, $message = 'A database error occured') {
-    parent::__construct($message);
-    $this->model = $model;
-  }
-
-  /**
-   * @see Error::to_object()
-   */
-  public function to_object () {
-    $object = parent::to_object();
-    $object['sql'] = array();
-
-    foreach ($this->model->queries as $key => $query) {
-      $object['sql'][] = array(
-        'query' => $query,
-        'error' => $this->model->errors[$key]
+    /**
+     * Converts all the error information into an array to be used in output
+     */
+    public function to_object () {
+      return array(
+        'type' => get_class($this),
+        'message' => $this->message
       );
     }
 
-    return $object;
+    /**
+     * Short description of the error
+     */
+    public function __toString () {
+      return $this->message;
+    }
+
   }
 
   /**
-   * @see Error::__toString()
+   * Used to abstract Model Errors
    */
-  public function __toString () {
-    $last_query = count($this->model->queries)-1;
-    return $this->message . "\n" .
-            "SQL: " . $this->model->queries[$last_query] . "\n" .
-            "Error: " . $this->model->errors[$last_query] . "\n";
+  class DatabaseError extends Error {
+    protected $model;
+
+    /**
+     * Initializes a DatabaseError
+     * @param {Model} $model An instance of Model on which the error occured
+     * @param {String} $message A custom description of the error
+     */
+    public function __construct ($model, $message = 'A database error occured') {
+      parent::__construct($message);
+      $this->model = $model;
+    }
+
+    /**
+     * @see Error::to_object()
+     */
+    public function to_object () {
+      $object = parent::to_object();
+      $object['sql'] = array();
+
+      foreach ($this->model->queries as $key => $query) {
+        $object['sql'][] = array(
+          'query' => $query,
+          'error' => $this->model->errors[$key]
+        );
+      }
+      
+      return $object;
+    }
+
+    /**
+     * @see Error::__toString()
+     */
+    public function __toString () {
+      $last_query = count($this->model->queries)-1;
+      return $this->message . "\n" .
+              "SQL: " . $this->model->queries[$last_query] . "\n" .
+              "Error: " . $this->model->errors[$last_query] . "\n";
+    }
   }
-}
 
 ?>
