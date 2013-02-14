a5795ded8ee21fff7060744821a8fdd43b239046
smirea <steven.mirea@gmail.com>
2013-02-14 15:51:12 +0100

    started working on admin pannel

---
+++ admin.php
@@ -0,0 +1,19 @@
+<html>
+  <head>
+    <link rel="stylesheet/less" type="text/css" href="css/voting-system.less">
+    <link rel="stylesheet/less" type="text/css" href="js/jquery-ui/jquery-ui.min.css">
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/less.min.js"></script>
+    <script type="text/javascript" src="js/jquery-ui/jquery-ui.min.js"></script>
+    <script type="text/javascript">
+      $(function on_admin_page_load () {
+
+      })();
+    </script>
+  </head>
+
+  <body>
+
+  </body>
+</html>
