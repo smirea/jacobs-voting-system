562fd2f86a6afb2d547b628c6e417974dc6f4ab5
Stefan Mirea <steven.mirea@gmail.com>
2013-02-14 15:19:02 +0100

    made it into an actual view

--- views/vote.php
+++ views/vote.php
@@ -1,7 +1,5 @@
 <?php
 
-  require_once 'config.php';
-  require_once 'utils.php';
   require_once 'classes/VoteModel.php';
   require_once 'classes/OptionModel.php';
 
@@ -10,18 +8,19 @@
 
   e_assert_isset($_GET, 'id,options');
 
-  $pid =  $_GET["id"];
+  $pid = $_GET["id"];
   $options = $_GET["options"];
   $uid = rand(); //$session['user_id'];
 
   if (!$voteModel->vote_is_valid($uid,$pid,$options)) {
-    output_error("Vote invalid");
+    Output::error("Vote invalid");
+    Output::done();
   }
 
   $voteModel->add_vote($pid,$uid);
 
   foreach ($options as $op) {
-    echo "Voted for " . $op . "\n";
+    Output::message("Voted for " . $op . "\n");
     $optionModel->increment_value($pid,$op);
   }
 
